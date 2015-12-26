70ae1dae1dbed0a110507abaf5ae87af5684b794
Alex J Burke <alex@alexjeffburke.com>
2015-12-26 17:48:38 +0000

    Add basic test suite.

---
+++ test/unexpectedEvents.js
@@ -0,0 +1,43 @@
+var EventEmitter = require('events').EventEmitter;
+var expect = require('unexpected');
+
+describe('unexpected-events', function () {
+    expect = expect.clone();
+    expect.installPlugin(require('../lib/unexpectedEvents'));
+
+    it('should error on an undefined value', function () {
+        var ev = new EventEmitter();
+
+        process.nextTick(function () {
+            ev.emit('foo', 'bar');
+        });
+
+        return expect(
+            expect(ev, 'for the first event on', 'foo', 'to equal')
+        , 'to be rejected');
+    });
+
+    it('should error on incomplete message', function () {
+        var ev = new EventEmitter();
+
+        process.nextTick(function () {
+            ev.emit('foo', 'bar');
+        });
+
+        return expect(
+            expect(ev, 'for the first event on', 'foo', 'to equal', ['bar', 'baz'])
+        , 'to be rejected');
+    });
+
+    it('should error on incomplete parts list', function () {
+        var ev = new EventEmitter();
+
+        process.nextTick(function () {
+            ev.emit('foo', 'bar', 'baz');
+        });
+
+        return expect(
+            expect(ev, 'for the first event on', 'foo', 'to equal', ['bar'])
+        , 'to be rejected');
+    });
+});
